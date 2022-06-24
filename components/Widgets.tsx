0843773d7998898f0db571ca9f66c4b0ee36bf72
Siddhartha Roy <sid86harth@gmail.com>
2022-06-24 07:03:29 +0000

    format components

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -4,7 +4,7 @@ import { TwitterTimelineEmbed } from 'react-twitter-embed'
 
 function Widgets() {
     return (
-        <div className='mt-2 px-2'>
+        <div className='mt-2 px-2 col-span-2 hidden lg:inline-block'>
             {/* Search Box */}
             <div className='flex items-center space-x-2 bg-gray-100 p-3 rounded-full mt-2'>
                 <SearchIcon className='h-5 w-5 text-gray-400' />
